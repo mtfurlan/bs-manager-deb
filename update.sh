96dc36847a1c8c27d73453f79aa44cea0b887955
silentrald <silentrald1216@gmail.com>
2024-12-25 15:51:40 +0800

    chore: added update.sh and put deb files in versions folder

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+keyname=${KEYNAME:?Must provide KEYNAME}
+
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+# Release, Release.gpg & InRelease
+apt-ftparchive release . > Release
+gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+
