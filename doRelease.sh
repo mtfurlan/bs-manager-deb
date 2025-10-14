0c764bb376fc73244f7f352ec5926f76ef01f84f
Mara Furland <mara@fur.land>
2025-10-14 12:54:16 -0400

    rewrite to do stuff from github release

---
+++ doRelease.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+debList=$(realpath "debsIncludedInThisRepo")
+
+# shellcheck disable=SC2120
+h () {
+    # if arguments, print them
+    [ $# == 0 ] || echo "$*"
+
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [OPTION]...
+  do a new release from $debList and push to latest github release
+Available options:
+  -h, --help       display this help and exit
+  -n, --dry-run    don't update release, only print and mess with the local tmp release dir
+EOF
+
+    # if args, exit 1 else exit 0
+    [ $# == 0 ] || exit 1
+    exit 0
+}
+
+die() {
+    local msg=$1
+    local code=${2-1} # default exit status 1
+    echo "$msg"
+    exit "$code"
+}
+
+# getopt short options go together, long options have commas
+TEMP=$(getopt -o hn --long help,dry-run -n "$0" -- "$@")
+#shellcheck disable=SC2181
+if [ $? != 0 ] ; then
+    die "something wrong with getopt"
+fi
+eval set -- "$TEMP"
+
+dry=false
+while true ; do
+    case "$1" in
+        -h|--help) h ;;
+        -n|--dry-run) dry=true; shift ;;
+        --) shift ; break ;;
+        *) die "issue parsing args, unexpected argument '$0'!" ;;
+    esac
+done
+keyname=${1:-}
+if [[ -z "$keyname" ]]; then
+    h "need to pass in gpg signing key name"
+fi
+
+echo "TODO do clean releases"
+#rm -rf release
+#mkdir release
+cd release
+
+# download all the debs in newFileList
+#< "$debList" xargs -n1 -P5 -- wcurl --curl-options="--create-dirs --silent"
+
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+# Release, Release.gpg & InRelease
+apt-ftparchive release . > Release
+echo "TODO: sign stuff again"
+#gpg --default-key "$keyname" -abs -o - Release > Release.gpg
+#gpg --default-key "$keyname" --clearsign -o - Release > InRelease
+
+if [ "$dry" = true ]; then
+    exit 0
+fi
+
+tag=latest
+release=latest
+
+git tag -f "$tag" -F- <<EOF
+tag thing I dunno
+
+to use this apt repo:
+curl -fsSL https://raw.githubusercontent.com/silentrald/bs-manager-deb/refs/heads/main/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/bs-manager.gpg
+curl -fsSL https://raw.githubusercontent.com/silentrald/bs-manager-deb/refs/heads/main/bs-manager.sources | sudo tee /etc/apt/sources.list.d/bs-manager.sources
+
+TODO: probably put list of debs in here? it's the release notes
+
+Date: $(date --iso=s)
+SHA: $(git rev-parse --short HEAD)
+EOF
+
+git push origin --force "$tag"
+
+
+# delete everything out of the release
+gh release view "$release" --json assets --jq '.assets[] | .name' | xargs --no-run-if-empty -n1 gh release delete-asset "$release"
+# update the release to point to the updated tag
+gh release edit "$release" -n "$(git tag -l --format='%(body)' "$tag")" -t "$(git tag -l --format='%(subject)' "$tag")"
+# upload new assets
+gh release upload "$release" ./*
--- update.sh
+++ update.sh
@@ -1,13 +1,90 @@
 #!/bin/bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-keyname=${KEYNAME:?Must provide KEYNAME}
+debList=$(realpath "debsIncludedInThisRepo")
+upstreamRepo=Zagrios/bs-manager
 
-# Packages & Packages.gz
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
+# shellcheck disable=SC2120
+h () {
+    # if arguments, print them
+    [ $# == 0 ] || echo "$*"
 
-# Release, Release.gpg & InRelease
-apt-ftparchive release . > Release
-gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
-gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [OPTION]...
+  update $debList with debs this repo is publishing and push a commit
+Available options:
+  -h, --help       display this help and exit
+  -n, --check      don't modify or push anything, just check if an update is required
+EOF
 
+    # if args, exit 1 else exit 0
+    [ $# == 0 ] || exit 1
+    exit 0
+}
+
+die() {
+    local msg=$1
+    local code=${2-1} # default exit status 1
+    echo "$msg"
+    exit "$code"
+}
+
+# getopt short options go together, long options have commas
+TEMP=$(getopt -o hc --long help,check -n "$0" -- "$@")
+#shellcheck disable=SC2181
+if [ $? != 0 ] ; then
+    die "something wrong with getopt"
+fi
+eval set -- "$TEMP"
+
+check=false
+while true ; do
+    case "$1" in
+        -h|--help) h ;;
+        -c|--check) check=true; shift ;;
+        --) shift ; break ;;
+        *) die "issue parsing args, unexpected argument '$0'!" ;;
+    esac
+done
+
+# list of updsream debs to deal with, newline separated
+generateFileList () {
+    repo=${1:?Must Provide Repo to generateFileList}
+    releaseCount=${2:-2}
+    gh release list -R "$repo" --exclude-drafts --exclude-pre-releases --limit "$releaseCount" --json tagName --jq '.[] | .tagName' | xargs -I{} gh release view '{}' -R "$repo" --json assets --jq '.assets[] | .url' | grep ".deb$"
+}
+
+
+newFiles=$(mktemp -t tmp.debGitRepoThing.XXXXXXXXXX,)
+trap 'rm -- "$newFiles"' EXIT
+
+generateFileList "$upstreamRepo" "$releaseCount" | sort > "$newFiles"
+
+if cmp -s "$debList" "$newFiles" ; then
+    echo "upstream unchagned from current, not doing anything"
+    exit 0
+fi
+
+echo "deb list updated:"
+cat "$debList"
+echo new
+cat "$newFiles"
+diff "$debList" "$newFiles"
+
+
+if [ "$check" = true ]; then
+    exit 0
+fi
+
+cp "$newFiles" "$debList"
+echo TODO update readme
+git add README.md "$debList"
+
+git commit -F- <<EOF
+update deb list
+
+run on $(date +"%Y-%m-%d %H:%M:%S %z")
+TODO better message
+EOF
+git push
